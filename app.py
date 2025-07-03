1c6e327be7dda551269b43bf1d802a00b03e0c23
Tatiana Torres <ttmtorres@gmail.com>
2025-07-03 14:03:42 +0100

    Atualizando dados

---
+++ app.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+import plotly.express as px
+import streamlit as st
+from pathlib import Path
+# Lendo os dados
+car_data = pd.read_csv('Data/vehicles.csv')
+
+
+
+# Botão para histograma
+hist_button = st.button('Criar histograma')
+
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Novo botão para gráfico de dispersão
+scatter_button = st.button('Criar gráfico de dispersão')
+
+if scatter_button:
+    st.write('Criando um gráfico de dispersão: preço vs odômetro')
+    fig2 = px.scatter(car_data, x="odometer", y="price", title="Preço vc Odômetro")
+    st.plotly_chart(fig2, use_container_width=True)
